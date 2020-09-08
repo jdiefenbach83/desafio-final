8f01157b328a7366550e4c1703fc7337db6c8776
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2020-09-08 11:06:50 -0300

    Melhoria para controle de mudança de períodos no componente PeriodSelector

--- client/src/components/PeriodSelector.js
+++ client/src/components/PeriodSelector.js
@@ -4,10 +4,14 @@ import M from 'materialize-css';
 import periodsHelper from '../helpers/periodsHelper';
 
 export default function PeriodSelector({ onChange }) {
+  const inputPeriodSelector = useRef(null);
+
   const [currentPeriod, setCurrentPeriod] = useState(
     periodsHelper.getCurrentPeriod()
   );
-  const inputPeriodSelector = useRef(null);
+
+  const [isLeftDisabled, setIsLeftDisable] = useState(false);
+  const [isRightDisabled, setIsRightDisable] = useState(false);
 
   useEffect(() => {
     M.AutoInit();
@@ -16,34 +20,47 @@ export default function PeriodSelector({ onChange }) {
   const periodsList = periodsHelper.createPeriodList();
 
   const handleChangePeriod = (event) => {
-    setCurrentPeriod(event.target.value);
-    onChange(event.target.value);
+    setPeriod(event.target.value);
   };
 
   const handleClickMinus = () => {
-    setPeriod(-1);
+    const newPeriod = getNewPeriod(-1);
+    setPeriod(newPeriod);
   };
 
   const handleClickPlus = () => {
-    setPeriod(1);
+    const newPeriod = getNewPeriod(1);
+    setPeriod(newPeriod);
   };
 
-  const setPeriod = (direction) => {
-    const selectInput = document.querySelector('#periodSelector');
-    const instance = M.FormSelect.getInstance(selectInput);
-
-    const index = periodsList.findIndex((item) => {
+  const getNewPeriod = (direction) => {
+    const currentIndex = periodsList.findIndex((item) => {
       return item.id === currentPeriod;
     });
 
-    const newPeriod = periodsList[index + direction * -1];
+    const newPeriod = periodsList.find((item) => {
+      return item.index === currentIndex + direction * -1;
+    });
 
-    if (!!newPeriod) {
-      setCurrentPeriod(newPeriod.id);
-      onChange(newPeriod.id);
-      instance.input.value = newPeriod.value;
-      inputPeriodSelector.current.focus();
-    }
+    return newPeriod.id;
+  };
+
+  const setPeriod = (newPeriod) => {
+    const period = periodsList.find((item) => {
+      return item.id === newPeriod;
+    });
+
+    setIsLeftDisable(periodsList.length === period.index + 1);
+    setIsRightDisable(period.index === 0);
+
+    setCurrentPeriod(period.id);
+    onChange(period.id);
+
+    const selectInput = document.querySelector('#periodSelector');
+    const instance = M.FormSelect.getInstance(selectInput);
+
+    instance.input.value = period.value;
+    inputPeriodSelector.current.focus();
   };
 
   return (
@@ -53,6 +70,7 @@ export default function PeriodSelector({ onChange }) {
           <button
             onClick={handleClickMinus}
             className="waves-effect waves-light btn"
+            disabled={isLeftDisabled}
           >
             {'<'}
           </button>
@@ -77,6 +95,7 @@ export default function PeriodSelector({ onChange }) {
           <button
             onClick={handleClickPlus}
             className="waves-effect waves-light btn"
+            disabled={isRightDisabled}
           >
             {'>'}
           </button>
